400c886cfabb37417718ededd1e20f63de80e477
Luis Gimenez <72755559+menezmethod@users.noreply.github.com>
2022-09-02 00:49:30 -0400

    fix[]: fixed ability to patch and delete users

--- src/st-auth-svc/pkg/services/auth.go
+++ src/st-auth-svc/pkg/services/auth.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"net/http"
-	"time"
 
 	"st-auth-svc/pkg/db"
 	"st-auth-svc/pkg/models"
@@ -31,7 +33,7 @@ func (s *Server) Register(_ context.Context, req *pb.RegisterRequest) (*pb.Regis
 		user.Email = v.Value
 	}
 	if v := req.GetPassword(); v != nil {
-		user.Password = utils.HashPassword(v.Value)
+		user.Password = v.Value
 	}
 	if v := req.GetFullName(); v != nil {
 		user.FullName = v.Value
@@ -39,7 +41,15 @@ func (s *Server) Register(_ context.Context, req *pb.RegisterRequest) (*pb.Regis
 	if v := req.GetRole(); v != nil {
 		user.Role = v.Value
 	}
-	user.TimeRegistered = time.Now()
+	if v := req.GetTimeRegistered(); v != nil {
+		if err := v.CheckValid(); err != nil {
+			err = fmt.Errorf("invalid TimeRegistered: %s%w", err.Error(), errors.New(""))
+			return nil, err
+		}
+		if t := v.AsTime(); !t.IsZero() {
+			user.TimeRegistered = v.AsTime()
+		}
+	}
 
 	if result := s.H.DB.Create(&user); result.Error != nil {
 		return &pb.RegisterResponse{
@@ -51,10 +61,12 @@ func (s *Server) Register(_ context.Context, req *pb.RegisterRequest) (*pb.Regis
 	return &pb.RegisterResponse{
 		Status: http.StatusCreated,
 		Data: &pb.User{
-			Id:       user.Id,
-			Email:    user.Email,
-			FullName: user.FullName,
-			Role:     user.Role,
+			Id:             user.Id,
+			Email:          user.Email,
+			Password:       user.Password,
+			FullName:       user.FullName,
+			Role:           user.Role,
+			TimeRegistered: timestamppb.New(user.TimeRegistered),
 		},
 	}, nil
 }
@@ -114,7 +126,7 @@ func (s *Server) Validate(_ context.Context, req *pb.ValidateRequest) (*pb.Valid
 	if result := s.H.DB.Where(&models.User{Email: claims.Email}).First(&user); result.Error != nil {
 		return &pb.ValidateResponse{
 			Status: http.StatusNotFound,
-			Error:  "user not found",
+			Error:  "User not found",
 		}, nil
 	}
 
@@ -128,12 +140,10 @@ func (s *Server) UpdateUser(_ context.Context, req *pb.UpdateUserRequest) (*pb.U
 	var user models.User
 
 	if result := s.H.DB.Where(&models.User{Email: req.Email.Value}).First(&user); result.Error == nil {
-		if req.Id != user.Id {
-			return &pb.UpdateUserResponse{
-				Status: http.StatusConflict,
-				Error:  "email already exists",
-			}, nil
-		}
+		return &pb.UpdateUserResponse{
+			Status: http.StatusConflict,
+			Error:  "email already exists",
+		}, nil
 	}
 
 	if req.GetEmail().Value != "" {
@@ -152,6 +162,15 @@ func (s *Server) UpdateUser(_ context.Context, req *pb.UpdateUserRequest) (*pb.U
 		user.Role = req.GetRole().Value
 	}
 
+	if v := req.GetTimeRegistered(); v != nil {
+		if err := v.CheckValid(); err != nil {
+			err = fmt.Errorf("invalid TimeRegistered: %s%w", err.Error(), errors.New(""))
+			return nil, err
+		}
+		if t := v.AsTime(); !t.IsZero() {
+			user.TimeRegistered = v.AsTime()
+		}
+	}
 	user.Id = req.GetId()
 
 	if result := s.H.DB.Updates(&user); result.Error != nil {
@@ -164,15 +183,23 @@ func (s *Server) UpdateUser(_ context.Context, req *pb.UpdateUserRequest) (*pb.U
 	return &pb.UpdateUserResponse{
 		Status: http.StatusCreated,
 		Data: &pb.User{
-			Id:       user.Id,
-			Email:    user.Email,
-			FullName: user.FullName,
-			Role:     user.Role,
+			Id:             user.Id,
+			Email:          user.Email,
+			FullName:       user.FullName,
+			Role:           user.Role,
+			TimeRegistered: timestamppb.New(user.TimeRegistered),
 		},
 	}, nil
 }
 
 func (s *Server) DeleteUser(_ context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
+
+	if result := s.H.DB.First(&models.User{}, req.Id); result.Error != nil {
+		return &pb.DeleteUserResponse{
+			Status: http.StatusNotFound,
+			Error:  result.Error.Error(),
+		}, nil
+	}
 
 	if result := s.H.DB.Where("ID IN (?)", req.Id).Delete(&models.User{}); result.Error != nil {
 		return &pb.DeleteUserResponse{
